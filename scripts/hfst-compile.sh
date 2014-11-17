8c5b19437c7fa319273a1d492dc38b1000bdbf53
Flammie Pirinen <flammie@iki.fi>
2014-11-17 17:58:30 +0000

    epsilon fix

--- scripts/hfst-compile.sh
+++ scripts/hfst-compile.sh
@@ -4,6 +4,6 @@ if test $# -lt 1 ; then
     exit 1
 fi
 
-hfst-txt2fst -v $1.model1.att -o $1.model1.hfst
-hfst-txt2fst -v $1.segs.att -o $1.segs.hfst
-hfst-txt2fst -v $1.sents.att -o $1.sents.hfst
+hfst-txt2fst -e 'ε' -v $1.model1.att -o $1.model1.hfst
+hfst-txt2fst -e 'ε' -v $1.segs.att -o $1.segs.hfst
+hfst-txt2fst -e 'ε' -v $1.sents.att -o $1.sents.hfst
